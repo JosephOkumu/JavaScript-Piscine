ab3aa24419751140324115ad90731e31f3409365
JosephOkumu <jokumu25@gmail.com>
2024-08-28 21:21:04 +0300

    return mininum and maximum values

---
+++ min-max.js
@@ -0,0 +1,15 @@
+function max(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    }else if (num2 > num1) {
+        return num2;
+    }
+}
+
+function min(num1, num2) {
+    if (num1 < num2) {
+        return num1;
+    }else if (num2 < num1) {
+        return num2;
+    }
+}
